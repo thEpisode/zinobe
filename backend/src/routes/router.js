66a7596803f24fdddc6fde69696997e39b198953
Camilo Rodriguez <camiepisode@outlook.com>
2020-05-06 15:53:11 -0500

    fix: removed unnecesary api routes

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,9 +1,6 @@
 const router = {
   api: [
     { httpRoute: '/status', route: '/routes/api/status/status.route', handler: 'get', method: 'GET', protected: false },
-    // Upload files
-    { httpRoute: '/upload/file', route: '/routes/api/upload/upload.route', handler: 'upload', method: 'POST', protected: false, isUpload: true },
-    { httpRoute: '/upload/bulk', route: '/routes/api/upload/upload.route', handler: 'bulk', method: 'POST', protected: false, isUpload: true },
     // Auth
     { httpRoute: '/login', route: '/routes/api/auth/auth.route', handler: 'login', method: 'POST', protected: false },
     { httpRoute: '/logout', route: '/routes/api/auth/auth.route', handler: 'logout', method: 'POST', protected: false },
