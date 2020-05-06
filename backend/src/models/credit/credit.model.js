c2ebaa960b8faa8891635ad88e4f62c7aed6488d
Camilo Rodriguez <camiepisode@outlook.com>
2020-05-06 16:22:07 -0500

    fix: included properties into getter

--- backend/src/models/credit/credit.model.js
+++ backend/src/models/credit/credit.model.js
@@ -46,7 +46,11 @@ class CreditModel extends baseModel {
       last_modification: this.last_modification.value || this.last_modification.type.default,
       last_user_modification: this.last_user_modification.value || this.last_user_modification.type.default,
       status: this.status.value || this.status.type.default,
-      name: this.name.value || this.name.type.default
+      name: this.name.value || this.name.type.default,
+      payment_deadline: this.payment_deadline || this.payment_deadline.default,
+      amount_requested: this.amount_requested || this.amount_requested.default,
+      amount: this.amount || this.amount.default,
+      user_id: this.user_id || this.user_id.default
     }
   }
 }
